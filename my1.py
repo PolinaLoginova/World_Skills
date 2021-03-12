0943ab6cb412bf0634ef7f2c9bac95250cf95ad7
PoliKk <44136508+PolinaLoginova@users.noreply.github.com>
2021-03-12 18:50:14 +0300

    Последний коммит

--- my1.py
+++ my1.py
@@ -21,7 +21,7 @@ bot = telebot.TeleBot("1553844540:AAEslNcUTcBhm_H6vlHn_YZs1WU1wQem0UU")
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, "Привет! Я бот, помогающий с выбором товара. Для получения справки введите комманду /help. Для регистрации введите комманду /reg")
+	bot.reply_to(message, "Привет! Я бот, помогающий с выбором товара. \n Для получения справки введите комманду /help. Для регистрации введите комманду /reg")
 
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
@@ -30,92 +30,92 @@ def send_welcome(message):
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     global tovarname
-    if message.text == 'Привет' or message.text == 'здравствуйте' or message.text == 'привет' or message.text == 'Здравтвуйте!'  or message.text == 'Здравтвуйте' or message.text == 'здравтвуйте!' or message.text == 'привет!' or message.text == 'Привет' or  message.text == 'прiвет!' or message.text == 'Привiт':
-	    bot.reply_to(message, 'Здравствуйте! Для получения справки введите комманду /help')
+    if message.text == 'Привет' or message.text == 'здравствуйте' or message.text == 'привет' or message.text == 'Здравствуйте!'  or message.text == 'Здравствуйте' or message.text == 'здравтвуйте!' or message.text == 'привет!' or message.text == 'Привет' or  message.text == 'прiвет!' or message.text == 'Привiт':
+	    bot.reply_to(message, 'Здравствуйте! \n Для получения справки введите комманду /help')
     elif message.text == 'Ты кто' or message.text == 'ты кто' or message.text == 'Кто ты' or message.text == 'кто ты':
-        bot.reply_to(message, 'Привет! Я бот, потогающий с выбором. Для получения справки введите комманду /help')
+        bot.reply_to(message, 'Привет! \n Я бот, потогающий с выбором. Для получения справки введите комманду /help')
     elif message.text == '/reg': #регистрация пользователя
-        bot.send_message(message.from_user.id, "Привет! Давайте запишем ваши данные для создания заказа. Как вас зовут?")
+        bot.send_message(message.from_user.id, "Привет! \n Давайте запишем ваши данные для создания заказа. \n Как вас зовут?")
         bot.register_next_step_handler(message, reg_name)
     elif message.text == 'пол 0-25':
         #global tovarname
-        bot.send_message(message.from_user.id, "Для вас могут подойти: " + str(tovarname[790]) + ' '+ str(tovarname[791]) + ' ' +  str(tovarname[792]) + ' ' + str(tovarname[793]) + 'Больше товаров можно увидеть в категории "пол"')
+        bot.send_message(message.from_user.id, "Для вас могут подойти: \n" + str(tovarname[790]) + '\n '+ str(tovarname[791]) + '\n ' +  str(tovarname[792]) + ' \n' + str(tovarname[793]) + '\n Больше товаров можно увидеть в категории "пол"')
 
     elif message.text == 'пол 25-50':
         #global tovarname
-        bot.send_message(message.from_user.id, "Для вас могут подойти: " + str(tovarname[846]) + ' '+ str(tovarname[847]) + ' ' +  str(tovarname[848]) + ' ' + str(tovarname[849]) + 'Больше товаров можно увидеть в категории "пол"')
+        bot.send_message(message.from_user.id, "Для вас могут подойти: \n" + str(tovarname[846]) + '\n '+ str(tovarname[847]) + '\n ' +  str(tovarname[848]) + '\n ' + str(tovarname[849]) + '\n Больше товаров можно увидеть в категории "пол"')
 
     elif message.text == 'пол 50-75':
         #global tovarname
-        bot.send_message(message.from_user.id, "Для вас могут подойти: " + str(tovarname[895]) + ' '+ str(tovarname[896]) + ' ' +  str(tovarname[897]) + ' ' + str(tovarname[898]) + 'Больше товаров можно увидеть в категории "пол"')
+        bot.send_message(message.from_user.id, "Для вас могут подойти: \n" + str(tovarname[895]) + '\n '+ str(tovarname[896]) + '\n ' +  str(tovarname[897]) + '\n ' + str(tovarname[898]) + '\n Больше товаров можно увидеть в категории "пол"')
 
     elif message.text == 'пол 75-200':
         #global tovarname
-        bot.send_message(message.from_user.id, "Для вас могут подойти: " + str(tovarname[945]) + ' '+ str(tovarname[946]) + ' ' +  str(tovarname[947]) + ' ' + str(tovarname[948]) + 'Больше товаров можно увидеть в категории "пол"')
+        bot.send_message(message.from_user.id, "Для вас могут подойти: \n" + str(tovarname[945]) + '\n '+ str(tovarname[946]) + '\n ' +  str(tovarname[947]) + '\n ' + str(tovarname[948]) + '\nБольше товаров можно увидеть в категории "пол"')
 
     elif message.text == 'аксессуары 0-25':
         #global tovarname
-        bot.send_message(message.from_user.id, "Для вас могут подойти: " + str(tovarname[0]) + ' '+ str(tovarname[1]) + ' ' +  str(tovarname[2]) + ' ' + str(tovarname[3]) + 'Больше товаров можно увидеть в категории "пол"')
+        bot.send_message(message.from_user.id, "Для вас могут подойти: \n" + str(tovarname[0]) + '\n '+ str(tovarname[1]) + '\n ' +  str(tovarname[2]) + ' \n' + str(tovarname[3]) + '\nБольше товаров можно увидеть в категории "пол"')
 
     elif message.text == 'аксессуары 25-50':
         #global tovarname
-        bot.send_message(message.from_user.id, "Для вас могут подойти: " + str(tovarname[88]) + ' '+ str(tovarname[89]) + ' ' +  str(tovarname[90]) + ' ' + str(tovarname[91]) + 'Больше товаров можно увидеть в категории "пол"')
+        bot.send_message(message.from_user.id, "Для вас могут подойти: \n" + str(tovarname[88]) + '\n '+ str(tovarname[89]) + '\n ' +  str(tovarname[90]) + ' \n' + str(tovarname[91]) + '\nБольше товаров можно увидеть в категории "пол"')
 
     elif message.text == 'аксессуары 50-75':
         #global tovarname
-        bot.send_message(message.from_user.id, "Для вас могут подойти: " + str(tovarname[159]) + ' '+ str(tovarname[160]) + ' ' +  str(tovarname[161]) + ' ' + str(tovarname[162]) + 'Больше товаров можно увидеть в категории "пол"')
+        bot.send_message(message.from_user.id, "Для вас могут подойти: \n" + str(tovarname[159]) + '\n '+ str(tovarname[160]) + '\n ' +  str(tovarname[161]) + '\n ' + str(tovarname[162]) + '\nБольше товаров можно увидеть в категории "пол"')
 
     elif message.text == 'аксессуары 75-200':
         #global tovarname
-        bot.send_message(message.from_user.id, "Для вас могут подойти: " + str(tovarname[262]) + ' '+ str(tovarname[263]) + ' ' +  str(tovarname[264]) + ' ' + str(tovarname[265]) + 'Больше товаров можно увидеть в категории "пол"')
+        bot.send_message(message.from_user.id, "Для вас могут подойти: \n" + str(tovarname[262]) + '\n '+ str(tovarname[263]) + '\n ' +  str(tovarname[264]) + '\n ' + str(tovarname[265]) + '\nБольше товаров можно увидеть в категории "пол"')
 
     elif message.text == 'двери 0-25':
         #global tovarname
-        bot.send_message(message.from_user.id, "Для вас могут подойти: " + str(tovarname[350]) + ' '+ str(tovarname[351]) + ' ' +  str(tovarname[352]) + ' ' + str(tovarname[353]) + 'Больше товаров можно увидеть в категории "пол"')
+        bot.send_message(message.from_user.id, "Для вас могут подойти: \n" + str(tovarname[350]) + '\n '+ str(tovarname[351]) + '\n ' +  str(tovarname[352]) + '\n ' + str(tovarname[353]) + '\nБольше товаров можно увидеть в категории "пол"')
 
     elif message.text == 'двери 25-50':
         #global tovarname
-        bot.send_message(message.from_user.id, "Для вас могут подойти: " + str(tovarname[367]) + ' '+ str(tovarname[368]) + ' ' +  str(tovarname[369]) + ' ' + str(tovarname[370]) + 'Больше товаров можно увидеть в категории "пол"')
+        bot.send_message(message.from_user.id, "Для вас могут подойти: \n" + str(tovarname[367]) + ' \n'+ str(tovarname[368]) + '\n ' +  str(tovarname[369]) + ' \n' + str(tovarname[370]) + '\nБольше товаров можно увидеть в категории "пол"')
 
     elif message.text == 'двери 50-75':
         #global tovarname
-        bot.send_message(message.from_user.id, "Для вас могут подойти: " + str(tovarname[385]) + ' '+ str(tovarname[386]) + ' ' +  str(tovarname[387]) + ' ' + str(tovarname[388]) + 'Больше товаров можно увидеть в категории "пол"')
+        bot.send_message(message.from_user.id, "Для вас могут подойти: \n" + str(tovarname[385]) + '\n '+ str(tovarname[386]) + '\n ' +  str(tovarname[387]) + '\n ' + str(tovarname[388]) + '\nБольше товаров можно увидеть в категории "пол"')
 
     elif message.text == 'двери 75-200':
         #global tovarname
-        bot.send_message(message.from_user.id, "Для вас могут подойти: " + str(tovarname[405]) + ' '+ str(tovarname[406]) + ' ' +  str(tovarname[407]) + ' ' + str(tovarname[408]) + 'Больше товаров можно увидеть в категории "пол"')
+        bot.send_message(message.from_user.id, "Для вас могут подойти: \n" + str(tovarname[405]) + '\n '+ str(tovarname[406]) + ' \n' +  str(tovarname[407]) + '\n ' + str(tovarname[408]) + '\nБольше товаров можно увидеть в категории "пол"')
 
     elif message.text == 'стены 0-25':
         #global tovarname
-        bot.send_message(message.from_user.id, "Для вас могут подойти: " + str(tovarname[1005]) + ' '+ str(tovarname[1006]) + ' ' +  str(tovarname[1007]) + ' ' + str(tovarname[1008]) + 'Больше товаров можно увидеть в категории "пол"')
+        bot.send_message(message.from_user.id, "Для вас могут подойти: \n" + str(tovarname[1005]) + '\n '+ str(tovarname[1006]) + '\n ' +  str(tovarname[1007]) + '\n ' + str(tovarname[1008]) + '\nБольше товаров можно увидеть в категории "пол"')
 
     elif message.text == 'стены 25-50':
         #global tovarname
-        bot.send_message(message.from_user.id, "Для вас могут подойти: " + str(tovarname[1013]) + ' '+ str(tovarname[1014]) + ' ' +  str(tovarname[1015]) + ' ' + str(tovarname[1016]) + 'Больше товаров можно увидеть в категории "пол"')
+        bot.send_message(message.from_user.id, "Для вас могут подойти: \n" + str(tovarname[1013]) + '\n '+ str(tovarname[1014]) + '\n ' +  str(tovarname[1015]) + '\n ' + str(tovarname[1016]) + '\nБольше товаров можно увидеть в категории "пол"')
 
     elif message.text == 'стены 50-75':
-        #global tovarname
-        bot.send_message(message.from_user.id, "Для вас могут подойти: " + str(tovarname[1018]) + ' '+ str(tovarname[1019]) + ' ' +  str(tovarname[1020]) + ' ' + str(tovarname[1021]) + 'Больше товаров можно увидеть в категории "пол"')
+        #global tovarnam
+        bot.send_message(message.from_user.id, "Для вас могут подойти: \n" + str(tovarname[1018]) + '\n '+ str(tovarname[1019]) + '\n ' +  str(tovarname[1020]) + '\n ' + str(tovarname[1021]) + '\nБольше товаров можно увидеть в категории "пол"')
 
     elif message.text == 'стены 75-200':
         #global tovarname
-        bot.send_message(message.from_user.id, "Для вас могут подойти: " + str(tovarname[1025]) + ' '+ str(tovarname[1026]) + ' ' +  str(tovarname[1027]) + ' ' + str(tovarname[1028]) + 'Больше товаров можно увидеть в категории "пол"')
+        bot.send_message(message.from_user.id, "Для вас могут подойти: \n" + str(tovarname[1025]) + '\n '+ str(tovarname[1026]) + '\n ' +  str(tovarname[1027]) + '\n ' + str(tovarname[1028]) + '\nБольше товаров можно увидеть в категории "пол"')
 
     elif message.text == 'ковры 0-25':
         #global tovarname
-        bot.send_message(message.from_user.id, "Для вас могут подойти: " + str(tovarname[432]) + ' '+ str(tovarname[433]) + ' ' +  str(tovarname[434]) + ' ' + str(tovarname[435]) + 'Больше товаров можно увидеть в категории "пол"')
+        bot.send_message(message.from_user.id, "Для вас могут подойти: \n " + str(tovarname[432]) + ' '+ str(tovarname[433]) + ' ' +  str(tovarname[434]) + ' ' + str(tovarname[435]) + '\nБольше товаров можно увидеть в категории "пол"')
 
     elif message.text == 'ковры 25-50':
         #global tovarname
-        bot.send_message(message.from_user.id, "Для вас могут подоbot.reply_to(message, message.text)йти: " + str(tovarname[527]) + ' '+ str(tovarname[528]) + ' ' +  str(tovarname[529]) + ' ' + str(tovarname[530]) + 'Больше товаров можно увидеть в категории "пол"')
+        bot.send_message(message.from_user.id, "Для вас могут подойти: \n" + str(tovarname[527]) + '\n '+ str(tovarname[528]) + '\n ' +  str(tovarname[529]) + '\n ' + str(tovarname[530]) + '\nБольше товаров можно увидеть в категории "пол"')
 
     elif message.text == 'ковры 50-75':
         #global tovarname
-        bot.send_message(message.from_user.id, "Для вас могут подойти: " + str(tovarname[608]) + ' '+ str(tovarname[609]) + ' ' +  str(tovarname[610]) + ' ' + str(tovarname[611]) + 'Больше товаров можно увидеть в категории "пол"')
+        bot.send_message(message.from_user.id, "Для вас могут подойти: \n " + str(tovarname[608]) + '\n '+ str(tovarname[609]) + '\n ' +  str(tovarname[610]) + '\n ' + str(tovarname[611]) + '\nБольше товаров можно увидеть в категории "пол"')
 
     elif message.text == 'ковры 75-200':
         #global tovarname
-        bot.send_message(message.from_user.id, "Для вас могут подойти: " + str(tovarname[703]) + ' '+ str(tovarname[704]) + ' ' +  str(tovarname[705]) + ' ' + str(tovarname[706]) + 'Больше товаров можно увидеть в категории "пол"')
+        bot.send_message(message.from_user.id, "Для вас могут подойти: \n " + str(tovarname[703]) + '\n '+ str(tovarname[704]) + '\n ' +  str(tovarname[705]) + '\n ' + str(tovarname[706]) + '\nБольше товаров можно увидеть в категории "пол"')
 
     else: bot.reply_to(message, 'Простите, я вас не понимаю. Попробуйте комманды /start или /help')
 
@@ -160,7 +160,7 @@ def reg_phonenumber(message):
     keyboard.add(key_yes)
     key_no = types.InlineKeyboardButton(text='Нет', callback_data='no')
     keyboard.add(key_no)
-    question = 'Вам ' + str(age) + ' лет? Вас зовут: ' + name + ' ' + surname + ' ' + secondname + '. Ваша почта и номер телефона: ' + email + ' ' + phonenumder + '?'
+    question = 'Вам ' + str(age) + ' лет? Вас зовут: ' + name + ' ' + surname + ' ' + secondname + '. \n Ваша почта и номер телефона: ' + email + ' ' + phonenumder + '?'
     bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
 
 
